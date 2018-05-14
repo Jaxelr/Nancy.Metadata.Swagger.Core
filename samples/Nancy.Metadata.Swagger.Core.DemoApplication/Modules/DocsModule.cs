da5a58090cfdd0fdd154ad16349e2be3d3f31d37
Jaxel Rojas <jaxelr@gmail.com>
2018-05-13 20:11:19 -0400

    Added Swagger.json to the samples that generate the api doc.

--- samples/Nancy.Metadata.Swagger.Core.DemoApplication/Modules/DocsModule.cs
+++ samples/Nancy.Metadata.Swagger.Core.DemoApplication/Modules/DocsModule.cs
@@ -7,7 +7,7 @@ namespace Nancy.Metadata.Swagger.DemoApplication.Modules
     public class DocsModule : SwaggerDocsModuleBase
     {
         public DocsModule(IRouteCacheProvider routeCacheProvider)
-            : base(routeCacheProvider, "/api/docs", "Sample API documentation", "v1.0", "localhost:17916", "/api", "http")
+            : base(routeCacheProvider, "/api/docs/swagger.json", "Sample API documentation", "v1.0", "localhost:17916", "/api", "http")
         {
             Get("/", async (x, ct) => await Task.Run(() => Response.AsRedirect("/index.html")));
         }
--- samples/Nancy.Metadata.Swagger.DemoApplication/Modules/DocsModule.cs
+++ samples/Nancy.Metadata.Swagger.DemoApplication/Modules/DocsModule.cs
@@ -6,7 +6,7 @@ namespace Nancy.Metadata.Swagger.DemoApplication.Modules
 {
     public class DocsModule : SwaggerDocsModuleBase
     {
-        public DocsModule(IRouteCacheProvider routeCacheProvider) : base(routeCacheProvider, "/api/docs", "Sample API documentation", "v1.0", "localhost:5000", "/api", "http")
+        public DocsModule(IRouteCacheProvider routeCacheProvider) : base(routeCacheProvider, "/api/docs/swagger.json", "Sample API documentation", "v1.0", "localhost:5000", "/api", "http")
         {
             Get["/", true] = async (x, ct) => await Task.Run(() => Response.AsRedirect("/Content/index.html"));
         }
