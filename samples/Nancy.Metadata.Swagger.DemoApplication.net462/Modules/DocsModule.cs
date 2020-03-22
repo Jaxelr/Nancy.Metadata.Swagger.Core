920705dd20e736b68a7741a64e1501877c71fb63
Jaxel Rojas <jaxelr@gmail.com>
2020-03-22 18:58:51 -0400

    feat: added samples targeting net framework 4.6.2.

---
+++ samples/Nancy.Metadata.Swagger.DemoApplication.net462/Modules/DocsModule.cs
@@ -0,0 +1,17 @@
+﻿using Nancy.Metadata.Swagger.Modules;
+using Nancy.Routing;
+using System.Threading.Tasks;
+
+namespace Nancy.Metadata.Swagger.DemoApplication.Modules
+{
+    public class DocsModule : SwaggerDocsModuleBase
+    {
+        public DocsModule(IRouteCacheProvider routeCacheProvider)
+            : base(routeCacheProvider, "/api/docs/swagger.json", "Sample API documentation", "v1.0", "localhost:17916", "/api", "http")
+        {
+            Get["/", true]  = async (_, __) =>
+                await Task.Run(() => Response.AsRedirect("/index.html"))
+                .ConfigureAwait(false);
+        }
+    }
+}
---
+++ samples/Nancy.Metadata.Swagger.DemoApplication.net462/Modules/RootModule.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using Nancy.Metadata.Modules;
+using Nancy.Metadata.Swagger.Core;
+using Nancy.Metadata.Swagger.DemoApplication.Model;
+using Nancy.Metadata.Swagger.Fluent;
+using Nancy.ModelBinding;
+
+namespace Nancy.Metadata.Swagger.DemoApplication.Modules
+{
+    public class RootModule : NancyModule
+    {
+        public RootModule() : base("/api")
+        {
+            Get["/hello"] = r => HelloWorld();
+            Get["/hello/{name}"] = r => Hello(r.name);
+            Post["/hello"] = r => HelloPost();
+            Post["hello/model"] = r => HelloModel();
+            Post["/hello/nestedmodel"] = r => HelloNestedModel();
+        }
+
+        private Response HelloNestedModel()
+        {
+            try
+            {
+                NestedRequestModel model = this.BindAndValidate<NestedRequestModel>();
+
+                if (!ModelValidationResult.IsValid)
+                {
+                    return Response
+                         .AsJson(new ValidationFailedResponseModel(ModelValidationResult))
+                         .WithStatusCode(HttpStatusCode.BadRequest);
+                }
+
+                var response = new SimpleResponseModel
+                {
+                    Hello = $"Hello, {model?.SimpleModel.Name}. We got your name from a nested object."
+                };
+
+                return Response.AsJson(response);
+            }
+            catch (ModelBindingException)
+            {
+                return Response
+                     .AsJson(new ValidationFailedResponseModel("Model Binding Failed with Exception."))
+                     .WithStatusCode(HttpStatusCode.BadRequest);
+            }
+            catch (NullReferenceException)
+            {
+                return Response
+                .AsJson(new ValidationFailedResponseModel("The body contains an invalid nested request model."))
+                .WithStatusCode(HttpStatusCode.InternalServerError);
+            }
+        }
+
+        private Response HelloModel()
+        {
+            try
+            {
+                SimpleRequestModel model = this.BindAndValidate<SimpleRequestModel>();
+
+                if (!ModelValidationResult.IsValid)
+                {
+                    return Response
+                         .AsJson(new ValidationFailedResponseModel(ModelValidationResult))
+                         .WithStatusCode(HttpStatusCode.BadRequest);
+                }
+
+                var response = new SimpleResponseModel
+                {
+                    Hello = $"Hello, {model.Name}."
+                };
+
+                return Response.AsJson(response);
+            }
+            catch (ModelBindingException)
+            {
+                return Response
+                     .AsJson(new ValidationFailedResponseModel("Model Binding Failed with Exception."))
+                     .WithStatusCode(HttpStatusCode.BadRequest);
+            }
+            catch (Exception ex)
+            {
+                return Response
+                .AsJson(new ValidationFailedResponseModel(ex.Message))
+                .WithStatusCode(HttpStatusCode.InternalServerError);
+            }
+        }
+
+        private Response HelloPost()
+        {
+            var response = new SimpleResponseModel
+            {
+                Hello = "Hello Post!"
+            };
+
+            return Response.AsJson(response);
+        }
+
+        private Response Hello(string name)
+        {
+            var response = new SimpleResponseModel
+            {
+                Hello = $"Hello, {name}."
+            };
+
+            return Response.AsJson(response);
+        }
+
+        private Response HelloWorld()
+        {
+            var response = new SimpleResponseModel
+            {
+                Hello = "Hello World!"
+            };
+
+            return Response.AsJson(response);
+        }
+    }
+
+    public class RootMetadataModule : MetadataModule<SwaggerRouteMetadata>
+    {
+        public RootMetadataModule()
+        {
+            Describe["SimpleRequest"] = desc => new SwaggerRouteMetadata(desc)
+                .With(i => i.WithResponseModel("200", typeof(SimpleResponseModel), "Sample response")
+                            .WithSummary("Simple GET example"));
+
+            Describe["SimpleRequestWithParameter"] = desc => new SwaggerRouteMetadata(desc)
+                .With(i => i.WithResponseModel("200", typeof(SimpleResponseModel), "Sample response")
+                            .WithRequestParameter("name")
+                            .WithSummary("Simple GET with parameters"));
+
+            Describe["SimplePostRequest"] = desc => new SwaggerRouteMetadata(desc)
+                .With(info => info.WithResponseModel("200", typeof(SimpleResponseModel), "Sample response")
+                    .WithSummary("Simple POST example"));
+
+            Describe["PostRequestWithModel"] = desc => new SwaggerRouteMetadata(desc)
+                .With(info => info.WithResponseModel("200", typeof(SimpleResponseModel))
+                    .WithResponseModel("400", typeof(ValidationFailedResponseModel))
+                    .WithSummary("Simple POST example with request model")
+                    .WithRequestModel(typeof(SimpleRequestModel)));
+
+            Describe["PostRequestWithNestedModel"] = desc => new SwaggerRouteMetadata(desc)
+                    .With(info => info.WithResponseModel("200", typeof(SimpleResponseModel))
+                    .WithResponseModel("400", typeof(ValidationFailedResponseModel))
+                    .WithSummary("Simple POST example with nested request model")
+                    .WithRequestModel(typeof(NestedRequestModel)));
+        }
+    }
+}
